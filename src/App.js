3bf4c541f7daa394f3bc14018a7668f8f68c17d7
Syeda Abdul Kareem <syedabdulkareem444@gmail.com>
2023-09-29 18:27:24 +0530

    gg

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
-import React from 'react';
-import Task1 from './task1/index'
-import Task2 from './task-2/index.js'
+import React from "react";
+import Task1 from "./task1/index";
+import Task2 from "./task-2/index.js";
+import Task3 from "./task-3/index.js";
 
 function App() {
-
   return (
-        <Router>
-            <Routes>
-              <Route path="/task2" element={<Task2/>} />
-              <Route path="/" element={<Task1/>} />
-            </Routes>
-        </Router>
+    <div>
+      <p>Navigate to task</p>
+      <Router>
+        <Routes>
+          <Route path="/task3" element={<Task3 />} />
+          <Route path="/task2" element={<Task2 />} />
+          <Route path="/task1" element={<Task1 />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
